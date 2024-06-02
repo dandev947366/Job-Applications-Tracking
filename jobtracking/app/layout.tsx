04541aff8afbf598a0b62ba9a51383478735b5e0
dandev947366 <dan.dev947366@gmail.com>
2024-06-03 00:08:29 +0300

    Clerk Middleware

--- jobtracking/app/layout.tsx
+++ jobtracking/app/layout.tsx
@@ -1,22 +1,28 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import {
+  ClerkProvider
+} from '@clerk/nextjs'
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Job Tracking",
-  description: "Job applications tracking system for job seekers",
+  description: "Job applications tracking system for job seekers"
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
+  children
+}:{
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+  
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
