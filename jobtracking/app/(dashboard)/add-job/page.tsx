992e2b246659ea5c80c49be724c4a736fd726868
dandev947366 <dan.dev947366@gmail.com>
2024-06-11 00:05:55 +0300

    AddJobForm Component

--- jobtracking/app/(dashboard)/add-job/page.tsx
+++ jobtracking/app/(dashboard)/add-job/page.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Heading from '@/components/typography/Heading'
 import AddJob from '@/components/add-job/AddJob'
+import FormComponents from '@/components/FormComponents'
+import AddJobForm from '@/components/add-job/AddJobForm'
 const AddJobPage = () => {
   return (
     <div>
-      <Heading>Add Job</Heading>
-    <AddJob  />
+    
+    <AddJobForm  />
+    
     </div>
   )
 }
